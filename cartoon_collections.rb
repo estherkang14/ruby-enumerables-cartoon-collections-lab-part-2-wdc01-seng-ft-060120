bf2528e79cbfba042cbec33167566c4e1808a8a8
Esther Kang <estherkang14@gmail.com>
2020-05-20 20:03:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   
   planeteer_calls.any? { |element|
-  element.to_i > 4 
+  element.to_i < 4 
   }
   
 end
