06eefdce66409e17197cbb760c4dba437c62d06f
Esther Kang <estherkang14@gmail.com>
2020-05-20 20:01:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   
   planeteer_calls.select { |element|
-  element >= 4 
+  element.to_i >= 4 
   }
   
 end
